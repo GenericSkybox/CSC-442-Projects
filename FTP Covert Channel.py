79ec34fef5a87e80baf65adf7965115a98b56895
eortiz <eric.ortiz@pioneerrx.com>
2018-03-26 13:32:20 -0500

    Added the FTP Covert Channel homework

---
+++ FTP Covert Channel.py
@@ -0,0 +1,10 @@
+import subprocess
+import os
+
+bashCommand = "cd ../.."
+bashCommand1 = "touch file1"
+bashCommand2 = "ls"
+
+os.system(bashCommand)
+os.system(bashCommand1)
+os.system(bashCommand2)
