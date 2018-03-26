5c46d68d497fcc504b8809de937c4aab469cc804
eortiz <eric.ortiz@pioneerrx.com>
2018-03-26 13:35:07 -0500

    Added website refs

--- FTP Covert Channel.py
+++ FTP Covert Channel.py
@@ -8,3 +8,9 @@ bashCommand2 = "ls"
 os.system(bashCommand)
 os.system(bashCommand1)
 os.system(bashCommand2)
+
+"""
+Website references
+https://stackoverflow.com/questions/4256107/running-bash-commands-in-python - os.system(bashCommand)
+https://stackoverflow.com/questions/40608183/how-to-make-a-python-script-to-download-a-file-from-a-ftp-server?rq=1
+"""
