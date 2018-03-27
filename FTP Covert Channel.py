6bfc34fa4f744a21f09fdb5e6884ff43b55bd1d0
Eric Ortiz <e.ortiz.programmer@gmail.com>
2018-03-26 19:35:34 -0500

    Trying a different approach with accessing the ftp server

--- FTP Covert Channel.py
+++ FTP Covert Channel.py
@@ -1,16 +1,23 @@
-import subprocess
 import os
+import ftplib
 
+ftp = ftplib.FTP("jeangourd.com")
+ftp.login("anonymous", "")
+
+
+"""
 bashCommand = "cd ../.."
 bashCommand1 = "touch file1"
 bashCommand2 = "ls"
+bashCommand3 = "rm file1"
 
 os.system(bashCommand)
 os.system(bashCommand1)
 os.system(bashCommand2)
+os.system(bashCommand3)
+"""
 
 """
 Website references
 https://stackoverflow.com/questions/4256107/running-bash-commands-in-python - os.system(bashCommand)
-https://stackoverflow.com/questions/40608183/how-to-make-a-python-script-to-download-a-file-from-a-ftp-server?rq=1
 """
