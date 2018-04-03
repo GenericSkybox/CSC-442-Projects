02ae8b252b71dc7c01f7ad74b6405b8ac2443e59
eortiz <eric.ortiz@pioneerrx.com>
2018-04-03 15:23:28 -0500

    Fixed up the decoder even more so that it's more readable and doesn't duplicate code as much. I know for a fact that it works in 7-bit, but I haven't tried 8-bit or mixed bit. However, it shouldn't be a problem to test

--- Binary Decoder.py
+++ Binary Decoder.py
@@ -5,11 +5,50 @@
 # Desc:  This program decodes binary into both 7-bit and 8-bit ASCII
 ####################################################################
 
-# library used for reading standard input
+# Library used for reading standard input
 import sys
 
+DEBUG = False
+
+# This function converts a string of binary (i.e. "tempdata") into an actual string, depending on the bit-type of the
+# binary (i.e. "n")
+def translate(tempdata, n):
+    # First define a list of characters that will later be concatenated together to form a sentence
+    charlist = []
+
+    # Although there's better ways to do it, we just need a loop that continues until the data string is empty
+    while True:
+        # We grab the first n (bit-type) characters of the data string, convert that binary bit into an integer, then
+        # convert that integer into a character
+        bchar = tempdata[:n]
+        bint = int(bchar, 2)
+        character = chr(bint)
+
+        # If the character is a backspace, then truncate the list by 1
+        if character == '\b':
+            charlist = charlist[:-1]
+        # Otherwise, append it to our list of characters
+        else:
+            charlist.append(character)
+
+        if DEBUG:
+            print(character)
+
+        # If there's more than n (bit-type) characters left in the data string, we "cut off" the first n characters of
+        # the string and store the rest
+        if (len(tempdata) > n):
+            tempdata = tempdata[n:]
+        # Else, we've gone through the entire string
+        else:
+            break
+
+    # Once we have our character list, we concatenate all of our characters into one string and print it
+    finalstring = ''.join(charlist)
+    print(finalstring)
+
+
 # START #
-# grab the data from the file, strip it of unnecessary characters, and determine the length of the binary number
+# Grab the data from the file, strip it of unnecessary characters, and determine the length of the binary number left
 data = sys.stdin.read()
 data = data.replace('\r\n', '')
 data = data.replace('\r', '')
@@ -17,126 +56,38 @@ data = data.replace('\n', '')
 data = data.replace(' ', '')
 datalen = len(data)
 
-# DEBUG: print the data we just collected and its length
-"""
-print(data)
-print(datalen)
-"""
+if DEBUG:
+    print(data)
+    print(datalen)
 
-# in case we need the binary later, store the data into a temporary variable
-tempdata = data
-# define a list of characters that will later be concatenated together to form a sentence
-charlist = []
-
-# if the length of the data is a multiple of 7 AND 8, then we cannot determine which bit-type of ASCII its written in
-# therefore, we'll just print both and have the user read it to determine
+# If the length of the data is a multiple of 7 AND 8, then we cannot determine which bit-type of ASCII its written in.
+# Therefore, we'll just print both and have the user read it to determine the message themselves
 if datalen % 8 == 0 and datalen % 7 == 0:
-    print("Binary string can be either 7-bit or 8-bit")
+    if DEBUG:
+        print("Binary string can be either 7-bit or 8-bit")
 
-    # although there's better ways to do it, we just need a loop that continues until the data string is empty
-    while True:
-        # we grab the first 8 characters of the data string and convert that binary bit into an integer
-        bchar = tempdata[:8]
-        bint = int(bchar, 2)
-        # then we convert the integer into a character
-        character = chr(bint)
+    # Since we don't know if the binary is 7-bit or 8-bit, we'll call translate twice with different bit-types
+    translate(data, 8)
+    translate(data, 7)
 
-        # if the character is a backspace, then truncate the list by 1
-        if character == '\b':
-            charlist = charlist[:-1]
-        # otherwise, append it to our list of characters
-        else:
-            charlist.append(character)
-
-        # DEBUG:
-        # print(character)
-
-        # if there's more than 8 characters left in the data string, "cut off" the first 8 characters of it and store it
-        if (len(tempdata) > 8):
-            tempdata = tempdata[8:]
-        # else, we've gone through the entire string
-        else:
-            break
-
-    # we concatenate all of our characters into one string and print that
-    finalstring = ''.join(charlist)
-    print(finalstring)
-
-    # since we don't know whether or not the characters are 7-bit or 8-bit, we'll do the same thing again, but for 7-bit
-    while True:
-        bchar = tempdata[:7]
-        bint = int(bchar, 2)
-        character = chr(bint)
-
-        if character == '\b':
-            charlist = charlist[:-1]
-        else:
-            charlist.append(character)
-
-        # DEBUG:
-        # print(character)
-
-        if (len(tempdata) > 7):
-            tempdata = tempdata[7:]
-        else:
-            break
-
-# if the data string is divisible by only 8, then the characters are 8-bit ASCII
+# If the data string is divisible by only 8, then the binary is 8-bit, and we will translate appropriately
 elif datalen % 8 == 0:
-    print("Binary string is 8-bit")
+    if DEBUG:
+        print("Binary string is 8-bit")
 
-    while True:
-        bchar = tempdata[:8]
-        bint = int(bchar, 2)
-        character = chr(bint)
+    translate(data, 8)
 
-        if character == '\b':
-            charlist = charlist[:-1]
-        else:
-            charlist.append(character)
-
-        # DEBUG:
-        # print(character)
-
-        if (len(tempdata) > 8):
-            tempdata = tempdata[8:]
-        else:
-            break
-
-# if the data string is divisible by only 7, then the characters are 7-bit ASCII
+# If the data string is divisible by only 7, then the binary is 7-bit, and we will translate appropriately
 elif datalen % 7 == 0:
-    print("Binary string is 7-bit")
+    if DEBUG:
+        print("Binary string is 7-bit")
 
-    while True:
-        bchar = tempdata[:7]
-        bint = int(bchar, 2)
-        character = chr(bint)
+    translate(data, 7)
 
-        if character == '\b':
-            charlist = charlist[:-1]
-        else:
-            charlist.append(character)
-
-        # DEBUG:
-        # print(character)
-
-        if (len(tempdata) > 7):
-            tempdata = tempdata[7:]
-        else:
-            break
-
-# otherwise, the string is a combination of both
+# Otherwise, the string is a combination of both... so error
 else:
-    print("Binary string is a combination of 7-bit and 8-bit")
+    if DEBUG:
+        print("Binary string is a combination of 7-bit and 8-bit")
+
     print("Error: multi-bit binary detected")
-
-# lastly, we concatenate all of our characters into one string and print that
-finalstring = ''.join(charlist)
-print(finalstring)
-
-"""
-First the program needs to read from standard input a string of binary
-Then we need to determine whether or not the binary is 7-bit, 8-bit, or both
-Regardless of the bit type, we need to translate each group of digits into characters
-Re-concatenate the characters into a string and print it
-"""
+    exit()
